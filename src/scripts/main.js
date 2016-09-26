5fe889aeab679413f8f2e86bf579b734889d4c09
Ghost-White <1964647370@qq.com>
2016-09-26 21:48:21 +0800

    增加小球为3排

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -52,71 +52,98 @@ $(function(){
 	};
 
 	//小球参数
-	var balls = $(".balls li");
-	var screenHeight = document.body.offsetHeight;
-	var ballHeight = balls[0].offsetHeight;
-	var ballsHeight = ballHeight*(balls.length);
-	var top_dis_u = Math.ceil((screenHeight-ballsHeight)/2);
+	var ball_array ;
+	var balls_0 ;
+	var balls_1 ;
+	var balls_2 ;
+	var screenHeight ;
+	var ballHeight ;
+	var ballsHeight ;
+	var top_dis_u ;
 
 	//初始化小球
 	var balls_unit = function(){
-		/*$(".balls").clone(true).appendTo($(".nav"));
-		$(".balls").clone(true).appendTo($(".nav"));*/
-		for(var i = 0;i<balls.length;i++){
-			var top_dis_ = top_dis_u+i*ballHeight+'px';
-			$(balls[i]).css({top:top_dis_});
+		$(".balls").clone(true).appendTo($(".nav"));
+		$(".balls").clone(true).appendTo($(".nav"));
+		$(".balls").eq(3).css({display:"none"});
+
+		ball_array = $(".balls");
+		balls_0 = $(ball_array[0]).find("li");
+		balls_1 = $(ball_array[1]).find("li");
+		balls_2 = $(ball_array[2]).find("li");
+		screenHeight = document.body.offsetHeight;
+		ballHeight = balls_0[0].offsetHeight;
+		ballsHeight = ballHeight*(balls_0.length);
+		top_dis_u = Math.ceil((screenHeight-ballsHeight)/2);
+
+		var left_dis_1 = '90px',
+			left_dis_2 = '30px';
+
+		for(var i = 0;i<balls_0.length;i++){
+			var top_dis_0 = (top_dis_u+(i-1)*ballHeight)+'px',
+				top_dis_1 = (top_dis_u+i*ballHeight)+'px',
+				top_dis_2 = (top_dis_u+(i+1)*ballHeight)+'px';
+
+			$(balls_0[i]).css({top:top_dis_0});
+			$(balls_1[i]).css({top:top_dis_1,left:left_dis_1});
+			$(balls_2[i]).css({top:top_dis_2,left:left_dis_2});
 		}	
 	};
 
 	//小球运动
-	var balls_action = function(obj){
-		var ball_action = function(i,step){
+	var balls_action = function(obj,k){
+		var ball_action = function(i,step){ 
+			
 			var top_dis_ = i*ballHeight+'px';
 			var bottom_dis_ = (screenHeight-ballsHeight+i*ballHeight)+'px';
-			var middle_dis_ = top_dis_u+i*ballHeight;
-			var time_,dis_;
+			var middle_dis_0 = (top_dis_u+(i-1)*ballHeight)+'px',
+				middle_dis_1 = (top_dis_u+i*ballHeight)+'px',
+				middle_dis_2 = (top_dis_u+(i+1)*ballHeight)+'px';
+			var time_,dis_,middle_dis__;
+			if(k===0){
+				middle_dis__ = middle_dis_0;
+			}else if(k===1){
+				middle_dis__ = middle_dis_1;
+			}else{
+				middle_dis__ = middle_dis_2;
+			}
+
 			if(step===1){
 				dis_ = top_dis_;
-				time_ = 500+i*100;
+				time_ = 500+(i+k)*100;
 			}else if(step===2){
 				dis_ = bottom_dis_;
-				time_ = 1000-i*100;
+				time_ = 1000-(i-k)*100;
 			}else if(step===3){
-				dis_ = middle_dis_;
-				time_ = 500+i*100;
+				dis_ = middle_dis__;
+				time_ = 500+(i+k)*100;
 			}
+			
 			$(obj[i]).animate({
 				top:dis_	
 			},time_);
 		};
 		var step_1 = function(){
-			ball_action(0,1);
-			ball_action(1,1);
-			ball_action(2,1);
-			ball_action(3,1);
-			ball_action(4,1);
+			for(var i=0 ;i<5;i++){
+				ball_action(i,1);
+			}
 		};
 		var step_2 = function(){
-			ball_action(0,2);
-			ball_action(1,2);
-			ball_action(2,2);
-			ball_action(3,2);
-			ball_action(4,2);
+			for(var i=0 ;i<5;i++){
+				ball_action(i,2);
+			}
 		};
 		var step_3 = function(){
-			ball_action(0,3);
-			ball_action(1,3);
-			ball_action(2,3);
-			ball_action(3,3);
-			ball_action(4,3);
+			for(var i=0 ;i<5;i++){
+				ball_action(i,3);
+			}
 		};
 		step_1();
-		setTimeout(step_2,1000);
-		setTimeout(step_3,2000);
-		setTimeout(keydown_event,2900);		//防止多次按键
-		setTimeout(slider_event,2900);		//防止多次点击螃蟹
+		setTimeout(step_2,1200);
+		setTimeout(step_3,2400);
+		// setTimeout(keydown_event,2900);		//防止多次按键
+		// setTimeout(slider_event,2900);		//防止多次点击螃蟹
 	};
-	
 	
 
 	//螃蟹摆动
@@ -164,19 +191,23 @@ $(function(){
 		});
 	};
 
-	var keydown_fun = function(){
+	var slider_fun = function(){
 		$(document).off('keydown');
 		$("body").off('click','.slider');
 		scroll_2("down");
-		balls_action(balls);
+		balls_action(balls_0,0);
+		balls_action(balls_1,1);
+		balls_action(balls_2,2);
+		setTimeout(keydown_event,3200);		//防止多次按键
+		setTimeout(slider_event,3200);		//防止多次点击螃蟹
 	};
 	var keydown_event = function(){
-		$(document).keydown(keydown_fun);
+		$(document).keydown(slider_fun);
 	};
 
 	var slider_event = function(){
 		$("body").on('click','.slider',function(){
-		keydown_fun();});
+		slider_fun();});
 	};
 
 	//入口头像
